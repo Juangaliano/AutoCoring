c9253811d349d35a4b2d6dec7ab5203142acfa1c
Juan <juangaliano.18@hotmail.com>
2024-07-09 19:22:22 -0300

    committ

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center w-full h-screen bg-black">
+        <img src="/home.jpg" alt="" className="h-auto max-w-full" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,42 @@
 function Navbar() {
   return (
-    <div className="relative flex items-center justify-between h-16 px-2 mx-auto bg-white max-w-7xl sm:px-6 lg:px-8">
+    <div className="relative flex items-center justify-between px-2 py-4 mx-auto bg-white h-28 max-full sm:px-6 lg:px-8">
       <div className="flex-shrink-0">
-        <img className="block w-auto h-8" src="/logosvg.svg" alt="Logo" />
+        <img className="block w-auto h-8 cursor-pointer" src="/logosvg.svg" alt="Logo"  />
       </div>
       <ul className="flex space-x-4">
-        <li className="px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-900">
-          Home
+        <li className="relative">
+          <a
+            href="#"
+            className="font-roboto text-left leading-7 tracking-normal font-semibold text-[#b2b2b2] text-base opacity-100 visible transition-opacity duration-300 ease-in-out py-2 hover:text-[#91C4FE] after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#91C4FE] after:transition-all after:duration-300 hover:after:w-full"
+          >
+            Inicio
+          </a>
         </li>
-        <li>Nosotros</li>
-        <li>Servicios</li>
-        <li>Contactanos</li>
+        <li className="transition-all ease-in-out border-b-2 border-transparent duration-300 hover:border-[#91C4FE]">
+          <a
+            href="#"
+            className="font-roboto font-semibold text-left leading-7 tracking-normal text-[#b2b2b2] text-base opacity-100 visible transition-opacity duration-300 ease-in-out py-2 hover:text-[#91C4FE]"
+          >
+            Nosotros
+          </a>
+        </li>
+        <li className="transition-all ease-in-out border-b-2 border-transparent duration-300 hover:border-[#91C4FE]">
+          <a
+            href="#"
+            className="font-roboto text-left leading-7 tracking-normal font-semibold text-[#b2b2b2] text-base opacity-100 visible transition-opacity duration-300 ease-in-out py-2 hover:text-[#91C4FE]"
+          >
+            Servicios
+          </a>
+        </li>
+        <li className="transition-all ease-in-out border-b-2 border-transparent duration-300 hover:border-[#91C4FE]">
+          <a
+            href="#"
+            className="font-roboto text-left leading-7 tracking-normal font-semibold text-[#b2b2b2] text-base opacity-100 visible transition-opacity duration-300 ease-in-out py-2 hover:text-[#91C4FE]"
+          >
+            Contacto
+          </a>
+        </li>
       </ul>
     </div>
   );
